29b0b80e1badbc352898de1712813b5952686050
Kevin Strinitz <kevin.strinitz@nx-technologies.com>
2023-09-21 12:26:49 +0200

    use python package to email validate

--- input_validation.py
+++ input_validation.py
@@ -1,12 +1,12 @@
 import re   #regular Expressions
-
+from email_validator import validate_email
 def is_valid_email(email:str) -> bool:
     """
     Uses regular expression to validate email.
 
     Returns:
-    True -- email is valid 
+    True -- email is valid
     False -- email is not valid
     """
-    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-    return re.match(pattern, email) is not None
+
+    return validate_email(email) is not None
--- test_input_validation.py
+++ test_input_validation.py
@@ -1,7 +1,7 @@
 import pytest
 from input_validation import is_valid_email
 
-@pytest.mark.parametrize("email", [
+@pytest.mark.parametrize("email_valid", [
     ("test@email.com")
 ,   ("t.est@email.com")
 ,   ("test@em.ail.com")
@@ -9,29 +9,60 @@ from input_validation import is_valid_email
 ,   ("te-st@email.com")
 ,   ("te_st@email.com")
 ,   ("test1@email.com")
+,   ("email@example.com")
+,   ("firstname.lastname@example.com")
+,   ("email@subdomain.example.com")
+,   ("firstname+lastname@example.com")
+,   ("email@123.123.123.123")
+,   ("email@[123.123.123.123]") 
+,   ("email@example.com") 
+,   ("1234567890@example.com") 
+,   ("email@example-one.com")
+,   ("_______@example.com")
+,   ("email@example.name")
+,   ("email@example.museum")
+,   ("email@example.co.jp")
+,   ("firstname-lastname@example.com")
 ])
-def test_is_valid_email__gueltige_Adressen(email):
+def test_is_valid_email__gueltige_Adressen(email_valid):
     # arrange
-    email_adress_to_be_tested = email
+    email_adress_to_be_tested = email_valid
     
     # act
-    response = is_valid_email(email)
+    response = is_valid_email(email_valid)
     
     # assert
     assert response is True
 
 
-@pytest.mark.parametrize("email", [
+@pytest.mark.parametrize("email_invalid", [
     ("testemail.com")   # Fehlendes @-Zeichen
 ,   ("test@email")      # Fehlende Top-Level-Domain
-,   ("test@em@ail.com") # Mehrfaches @-Zeichen
+,   ("test@em@ail.com") # Mehrfaches @-Zeichen,
+,   ("plainaddress") 
+,   ("#@%^%#$@#$@#.com"),
+    ("@example.com"), 
+    ("Joe Smith <email@example.com>"), 
+    ("email.example.com"), 
+    ("email@example@example.com"), 
+    (".email@example.com"), 
+    ("email.@example.com"), 
+    ("email..email@example.com"), 
+    ("あいうえお@example.com"), 
+    ("email@example.com (Joe Smith)"), 
+    ("email@example"), 
+    ("email@-example.com"), 
+    ("email@example.web"), 
+    ("email@111.222.333.44444"), 
+    ("email@example..com"), 
+    ("Abc..123@example.com"),
 ])
-def test_is_valid_email__ungueltige_Adressen(email):
+def test_is_valid_email__ungueltige_Adressen(email_invalid):
     # arrange
-    email_adress_to_be_tested = email
+    email_adress_to_be_tested = email_invalid
     
     # act
-    response = is_valid_email(email)
+    response = is_valid_email(email_invalid)
     
     # assert
     assert response is False
