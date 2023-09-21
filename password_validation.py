15ee5ddac6c4daef5a4603fd7c2bd7057e8d5a4d
Jerome Graf <jerome.graf@obi.de>
2023-09-21 14:19:22 +0200

    add password test

---
+++ password_validation.py
@@ -0,0 +1,11 @@
+import re
+
+def password_validator(password)-> [bool,str]:
+    pass_pattern = r'^(?=.*[A-Za-z0-9])[A-Za-z0-9 ?!%+$]+$'
+    if len(password) < 8: 
+        return False,"Password is too short"
+    if not re.match(pass_pattern, password):
+        return False,"Password is not valid, plese use only letters, numbers and the following special characters: ?!%+…"
+    if not re.search("[a-z,A-Z]", password): 
+        return False,"Password is not valid, please use at least one lowercase letter and one uppercase letter"
+    return True,"Password is valid"
---
+++ test_input_validation_password.py
@@ -0,0 +1,41 @@
+import pytest
+from password_validation import password_validator
+
+@pytest.mark.parametrize("password_valid", [
+    ("test12345"),
+#    ("test1234!"),
+#    ("test1234!$"),
+#    ("test1234!$%"),
+#    ("test1234!$%&"),
+#    ("test1234!$%&/"),
+#    ("test1234!$%&/()")
+]
+)
+
+def test_password_validator(password_valid):
+    response = password_validator(password_valid)
+    assert response[0] is True,"Password is valid"
+
+@pytest.mark.parametrize("password_too_short", [
+    ("short"),
+]
+)
+
+def test_password_validator_not_valid(password_too_short):
+    response = password_validator(password_too_short)
+    assert response[0] is False,"Password is too short"
+
+def test_password_validator_not_valid_chars(password_not_valid_chars):
+    password = "###############"
+    response = password_validator(password_not_valid_chars)
+    assert response[0] is False,"Password is not valid, plese use only letters, numbers and the following special characters: ?!%+$"
+
+def test_password_validator_not_one_uppercase():
+    password = "test1234!$%&/()"
+    response = password_validator(password)
+    assert response[0] is False,"Password is not valid, please use at least one lowercase letter and one uppercase letter"
+
+def test_password_validator_not_one_lowercase():
+    password = "TESST1234!$%&/()"
+    response = password_validator(password)
+    assert response[0] is False,"Password is not valid, please use at least one lowercase letter and one uppercase letter"
